c30f2aa1482c0003c186feed9ae206dcd2c4e5e3
Christopher Sands <chrstphrsands25@gmail.com>
2019-07-09 14:54:02 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Form, Input, Button, Select} from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const {Option} = Select;
+
+class App extends React.Component {
+    render() {
+
+        const formItemLayout = {
+            labelCol: {
+                xs: {span: 24},
+                sm: {span: 8},
+            },
+            wrapperCol: {
+                xs: {span: 24},
+                sm: {span: 16},
+            },
+        };
+
+        const {getFieldDecorator} = this.props.form;
+
+        return (
+            <Form {...formItemLayout} onSubmit={this.handleSubmit} style={{width: "75%"}}>
+                <Form.Item label="Nombres">
+                    {getFieldDecorator('nombres', {
+                        rules: [
+                            {
+                                required: true,
+                                message: 'Por favor ingrese sus nombres',
+                            },
+                        ],
+                    })(<Input/>)}
+                </Form.Item>
+                <Form.Item label="Apellido Paterno">
+                    {getFieldDecorator('paterno', {
+                        rules: [
+                            {
+                                required: true,
+                                message: 'Por favor ingrese su apellido paterno',
+                            },
+                        ],
+                    })(<Input/>)}
+                </Form.Item>
+                <Form.Item label="Apellido Materno">
+                    {getFieldDecorator('materno', {
+                        rules: [
+                            {
+                                required: true,
+                                message: 'Por favor ingrese su apellido materno',
+                            },
+                        ],
+                    })(<Input/>)}
+                </Form.Item>
+                <Form.Item label="Sexo">
+                    <Select placeholder={"Seleccione su sexo"}>
+                        <Option value="M">Masculino</Option>
+                        <Option value="F">Femenino</Option>
+                    </Select>
+                </Form.Item>
+
+            </Form>
+        )
+    }
 }
 
-export default App;
+export default Form.create()(App);
